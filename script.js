9e1570ee4f44a0b3aaeb303d87b475e47198f272
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-11-03 16:35:20 +0100

    Added specific info ab product

--- script.js
+++ script.js
@@ -59,25 +59,28 @@ btnAll.addEventListener("click", () => {
 })
 
 
-// TESTING MODAL
+
+
 // Get the modal
-var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
+//var btn = document.getElementById('btn-modal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+//var img = document.getElementById("myImg");
+//var modalImg = document.getElementById("img01");
+//var captionText = document.getElementById("caption");
+
+//img.onclick = function(){
+  //modal.style.display = "block";
+  //modalImg.src = this.src;
+  //captionText.innerHTML = this.alt;
+//}
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+//span.onclick = function() {
+  //modal.style.display = "none";
+//}
 
